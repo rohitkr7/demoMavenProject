f5731f1934e2258c8c0955ea7ac70e26dd5d367e
rohitkumar.roy <rohitkumar.roy@servicenow.com>
2021-05-10 16:52:56 +0530

    modified the failure message

--- src/test/java/DevOpsDemo/Calculator/CalculatorTest.java
+++ src/test/java/DevOpsDemo/Calculator/CalculatorTest.java
@@ -10,6 +10,6 @@ public class CalculatorTest {
 	public void sumTest() {
 		//This test will check the sum functionality of the test
 		
-		assertTrue("sum function not working fine", calculator.sum(10, 20) == 30);
+		assertTrue("Sum function not working fine.", calculator.sum(10, 20) == 30);
 	}
 }
