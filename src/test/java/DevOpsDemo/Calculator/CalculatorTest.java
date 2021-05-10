bb65c625b6211a71f1ef27cc4565aa3aadee5631
rohitkumar.roy <rohitkumar.roy@servicenow.com>
2021-05-10 16:41:59 +0530

    adding test class for Calculator

---
+++ src/test/java/DevOpsDemo/Calculator/CalculatorTest.java
@@ -0,0 +1,10 @@
+package DevOpsDemo.Calculator;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	@Test
+	public void sumTest() {
+		//This test will check the sum functionality of the test
+	}
+}
