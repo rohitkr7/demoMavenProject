a551608c5ad129bed473734d4dd5e4359e6a5cba
rohitkumar.roy <rohitkumar.roy@servicenow.com>
2021-05-10 09:27:51 +0530

    adding calculator java file

---
+++ src/main/java/DevOpsDemo/Calculator/Calulator.java
@@ -0,0 +1,19 @@
+package DevOpsDemo.Calculator;
+
+public class Calulator {
+	public double sum(double x, double y) {
+		return x + y; 
+	}
+	
+	public double subtract(double x, double y) {
+		return x - y; 
+	}
+	
+	public double multiply(double x, double y) {
+		return x * y; 
+	}
+	
+	public double divide(double x, double y) {
+		return x / y; 
+	}
+}
