949ad0ea0ba63e9c12c0ccbb1e0a76d52744598b
rohitkumar.roy <rohitkumar.roy@servicenow.com>
2022-02-17 21:41:05 +0530

    Adding PMC scripts

---
+++ install_hooks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Installs the hooks into your githooks directory
+
+printf "This script will copy the following hooks to your .git/hooks directory:\n\n"
+for hook in $(ls .hooks)
+do
+  echo $hook
+done
+printf "\n\n"
+read -r -p "Please enter Y to continue: " RESPONSE
+
+if [[ "$RESPONSE" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    cd .hooks
+    for hook in $(ls)
+    do
+        cp $hook ../../.git/hooks
+    done
+    cd ..
+fi
+
+echo "Hooks installed successfully"
