1ffe8c87d45a67baebc0598da4678a55ef5817aa
rohitkr7 <rohitkrroy06@gmail.com>
2022-05-15 14:14:13 +0530

    TEST: cleaned up unwanted code

--- .githooks/helperScripts/fileChecks.sh
+++ .githooks/helperScripts/fileChecks.sh
@@ -1,47 +1,29 @@
 # ----------------- Logic for gathering the list of files modified -----------------
 # command to get list of files which are modified
+clear
 str=$(git diff --name-only $(git merge-base master HEAD))
-echo 'List of modified files: ------------------>> '
+echo 'List of modified files: ---------------------------------------------------------------->> '
 separator=" "
 modifiedFilesList=(${str//${separator}/ })
 for mf in "${modifiedFilesList[@]}"
 do
     printf "$mf\n"
 done
-printf "\n------------------------------------------\n"
-
-# command to get list of files which are untracked
-str1=$(git ls-files --others --exclude-standard)
-echo 'List of untracked files: ------------------>> '
-untrackedFilesList=(${str1//${separator}/ })
-for utf in "${untrackedFilesList[@]}"
-do
-    printf "$utf\n"
-done
-printf "\n------------------------------------------\n"
-
-# command to get list of staged files
-str3=$(git diff --name-only --cached)
-echo 'List of stagged files: ------>> '
-stagedFilesList=(${str3//${separator}/ })
-for stg in "${stagedFilesList[@]}"
-do
-    printf "$stg\n"
-done
+printf "\n------------------------------------------------------------------------------------\n"
 
 # ----------------- Filter out files as per their extension and run checks -----------------
 ################## JAVA ##################
 # Rule1: Methods of the Test class should be declared as private.
 PREV_IFS=$IFS
 
-echo 'List of modifed Java files: ------------------>> '
+echo 'Scanning through all the Java files: --------------------------------------------------->> '
 for mf in "${modifiedFilesList[@]}"
 do
     prevLineWasDash=true
     prevLineAnnotationPresent=false
     if [[ "$mf" == *java ]];
     then
-        printf "$mf\n"
+        #printf "$mf\n"
         # Read the file content in a variable and print
         #value=`cat $mf`
         #echo "$value"
@@ -76,13 +58,13 @@ do
             fi
             if [[ $prevLineWasDash == false && $prevLineAnnotationPresent == false ]];
             then
-                printf "[WARNING] >> $mLine ***** please update to private access modifier instead of public *****\n"
+                printf "[WARNING] File: $mf >> $mLine ***** please update to private access modifier instead of public *****\n"
             fi
         done
+        printf "\n------------------------------------------------------------------------------------\n"
     fi
-    printf "\n------------------------------------------\n"
 done
-printf "\n------------------------------------------\n"
+printf "\n------------------------------------------------------------------------------------\n"
 
 # ------------------------------------------------------------------------------------ #
 # Rule4: Story number, Defect number, Testcase id should be mentioned
@@ -99,7 +81,7 @@ do
         fi
     fi
 done
-printf "\n------------------------------------------\n"
+printf "\n------------------------------------------------------------------------------------\n"
 
 
 
