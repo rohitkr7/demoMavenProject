8b3bc633e56f50e932b0fff04c87804a2a784297
rohitkr7 <rohitkrroy06@gmail.com>
2022-05-15 06:34:39 +0530

    TEST: modified filesChecks to warn for uneccessary public methods in java files

--- .githooks/helperScripts/fileChecks.sh
+++ .githooks/helperScripts/fileChecks.sh
@@ -1,9 +1,10 @@
+# ----------------- Logic for gathering the list of files modified -----------------
 # command to get list of files which are modified
-str=$(git diff --name-only $(git merge-base hooksFeature HEAD))
+str=$(git diff --name-only $(git merge-base master HEAD))
 echo 'List of modified files: ------------------>> '
 separator=" "
-arr=(${str//${separator}/ })
-for mf in "${arr[@]}"
+modifiedFilesList=(${str//${separator}/ })
+for mf in "${modifiedFilesList[@]}"
 do
     printf "$mf\n"
 done
@@ -12,28 +13,73 @@ printf "\n------------------------------------------\n"
 # command to get list of files which are untracked
 str1=$(git ls-files --others --exclude-standard)
 echo 'List of untracked files: ------------------>> '
-arr1=(${str1//${separator}/ })
-for utf in "${arr1[@]}"
+untrackedFilesList=(${str1//${separator}/ })
+for utf in "${untrackedFilesList[@]}"
 do
     printf "$utf\n"
 done
 printf "\n------------------------------------------\n"
 
-# command to get list of files which are either untracked or modified
-str2=$(git ls-files --modified --others --exclude-standard)
-echo 'List of untracked or modified files: ------>> '
-arr2=(${str2//${separator}/ })
-for utmf in "${arr2[@]}"
-do
-    printf "$utmf\n"
-done
-printf "\n------------------------------------------\n"
-
 # command to get list of staged files
 str3=$(git diff --name-only --cached)
-echo 'List of staged files: ------>> '
-arr3=(${str3//${separator}/ })
-for stg in "${arr3[@]}"
+echo 'List of stagged files: ------>> '
+stagedFilesList=(${str3//${separator}/ })
+for stg in "${stagedFilesList[@]}"
 do
     printf "$stg\n"
 done
+
+# ----------------- Filter out files as per their extension and run checks -----------------
+################## JAVA ##################
+PREV_IFS=$IFS
+prevLineWasDash=true
+prevLineAnnotationPresent=false
+
+echo 'List of modifed Java files: ------------------>> '
+for mf in "${modifiedFilesList[@]}"
+do
+    if [[ "$mf" == *java ]];
+        then
+            printf "$mf\n"
+            # Read the file content in a variable and print
+            #value=`cat $mf`
+            #echo "$value"
+            IFS=$'\n'
+            # grep -nB 1 "public.*[\(]" "$mf"
+
+            # Find matching lines in the file using grep command and a pattern
+            methodLists=($(grep -nB 1 "public.*[\(]" "$mf"))
+
+            for mLine in ${methodLists[@]}
+            do
+                #printf $mLine
+                if [[ $mLine == '--'* ]];
+                then
+                    prevLineWasDash=true
+                    prevLineAnnotationPresent=false
+                    continue
+                fi
+                if [[ $prevLineWasDash == true ]]
+                then
+                    printf 'inside prevLineWasDash == false block\n'
+                    if [[ $mLine == *'@Test'* ]];
+                    then
+                        printf "line starts with @Test\n"
+                        prevLineAnnotationPresent=true
+                    else
+                        printf "line does not starts with @Test\n"
+                        prevLineAnnotationPresent=false
+                    fi
+                    prevLineWasDash=false
+                    continue
+                fi
+                if [[ $prevLineWasDash == false && $prevLineAnnotationPresent == false ]];
+                then
+                    printf "[WARNING] >> $mLine\n"
+                fi
+            done
+    fi
+done
+printf "\n------------------------------------------\n"
+
+IFS=$PREV_IFS
