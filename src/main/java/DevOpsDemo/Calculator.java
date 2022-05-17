546f0473136bddec64598f362c5c232e34f40c1c
rohitkr7 <rohitkrroy06@gmail.com>
2022-05-17 08:06:44 +0530

    TEST: fixing comilation errors

--- src/main/java/DevOpsDemo/Calculator.java
+++ src/main/java/DevOpsDemo/Calculator.java
@@ -2,21 +2,21 @@ package DevOpsDemo;
 
 public class Calculator {
 	public double sum(double x, double y) {
-		return x + y; 
+		return x + y;
 	}
-	
+
 	public double subtract(double x, double y) {
-		return x - y; 
+		return x - y;
 	}
-	
+
 	public double multiply(double x, double y) {
-		return x * y; 
+		return x * y;
 	}
-	
+
 	public int divide(int x, int y) throws ArithmeticException {
 		//This is a modified division method
 		if(y!=0) {
-			return x / y; 
+			return x / y;
 		}
 		else {
 			throw new ArithmeticException("Divide By Zero");
