914c8e2b7d0265134a3436f37f8b1d64e2c48ad8
rohitkr7 <rohitkrroy06@gmail.com>
2022-07-30 09:24:53 +0530

    TEST: Changes for generating pmc log file under target folder

--- .githooks/helperScripts/fileChecks.sh
+++ .githooks/helperScripts/fileChecks.sh
@@ -1,15 +1,19 @@
 # ----------------- Logic for gathering the list of files modified -----------------
 # command to get list of files which are modified
 clear
+rm -rf target/PMC_FileScanLogs.txt
 str=$(git diff --name-only $(git merge-base master HEAD) & git ls-files --others --exclude-standard)
-echo 'List of modified files: ---------------------------------------------------------------->> '
+echo 'List of modified files: ---------------------------------------------------------->> '
+echo 'List of modified files: ---------------------------------------------------------->> ' >> target/PMC_FileScanLogs.txt
 separator=" "
 modifiedFilesList=(${str//${separator}/ })
 for mf in "${modifiedFilesList[@]}"
 do
     printf "$mf\n"
+    echo "$mf\n" >> target/PMC_FileScanLogs.txt
 done
 printf "\n------------------------------------------------------------------------------------\n"
+echo '\n------------------------------------------------------------------------------------\n' >> target/PMC_FileScanLogs.txt
 
 # ----------------- Filter out files as per their extension and run checks -----------------
 ################## JAVA ##################
@@ -17,8 +21,11 @@ printf "\n----------------------------------------------------------------------
 PREV_IFS=$IFS
 IFS=$'\n'
 
-echo 'Scanning through all the Modified Java files: --------------------------------------------------->> '
+echo 'Scanning through all the Modified Java files: ------------------------------------->> '
+echo 'Scanning through all the Modified Java files: ------------------------------------->> ' >> target/PMC_FileScanLogs.txt
 echo 'Rule - 1: Methods of the Test class should be declared as private ===>>\n'
+echo 'Rule - 1: Methods of the Test class should be declared as private ===>>\n' >> target/PMC_FileScanLogs.txt
+
 for mf in "${modifiedFilesList[@]}"
 do
     prevLineWasDash=true
@@ -62,16 +69,18 @@ do
             if [[ $prevLineWasDash == false && $prevLineAnnotationPresent == false ]];
             then
                 printf "[WARNING] File: $mf >> $mLine ***** please update to private access modifier instead of public *****\n"
+                echo "[WARNING] File: $mf >> $mLine ***** please update to private access modifier instead of public *****\n" >> target/PMC_FileScanLogs.txt
             fi
         done
         printf "\n------------------------------------------------------------------------------------\n"
+        echo "\n------------------------------------------------------------------------------------\n" >> target/PMC_FileScanLogs.txt
     fi
 done
-printf "\n------------------------------------------------------------------------------------\n"
 
 # ------------------------------------------------------------------------------------ #
 # Rule - 4: Story number, Defect number, Testcase id should be mentioned
 printf 'Rule - 4: Story number, Defect number, Testcase id should be mentioned ===>> \n\n'
+echo 'Rule - 4: Story number, Defect number, Testcase id should be mentioned ===>> \n\n' >> target/PMC_FileScanLogs.txt
 for mf in "${modifiedFilesList[@]}"
 do
     # check if file exists and extension is java
@@ -80,13 +89,16 @@ do
         if ! grep -q "@Story(\|@TestCase(\|@Defect(" "$mf"
         then
             echo '[WARNING] @Story/@TestCase/@Defect not found in '$mf
+            echo '[WARNING] @Story/@TestCase/@Defect not found in '$mf >> target/PMC_FileScanLogs.txt
         fi
     fi
 done
 printf "\n------------------------------------------------------------------------------------\n"
+echo "\n------------------------------------------------------------------------------------\n" >> target/PMC_FileScanLogs.txt
 
 # Rule - 8: If Java file has Test Method then the file name should end with *IT.java or *UIIT.java pattern
 printf 'Rule - 8: Test Java file name checks ====>\n\n'
+echo 'Rule - 8: Test Java file name checks ====>\n\n' >> target/PMC_FileScanLogs.txt
 for mf in "${modifiedFilesList[@]}"
 do
     # check if file exists and extension is java
@@ -96,13 +108,16 @@ do
         if grep -q "@Story(\|@TestCase(\|@Defect(\|@Test\|@Step" "$mf"
         then
             echo '[WARNING] @Story/@TestCase/@Defect/@Test/@Step found in '$mf' but file name does not end with IT or UIIT\n'
+            echo '[WARNING] @Story/@TestCase/@Defect/@Test/@Step found in '$mf' but file name does not end with IT or UIIT\n'  >> target/PMC_FileScanLogs.txt
         fi
     fi
 done
 printf "\n------------------------------------------------------------------------------------\n"
+echo "\n------------------------------------------------------------------------------------\n" >> target/PMC_FileScanLogs.txt
 
 # Rule - 7: Constants should be upper case
 printf 'Rule - 7: Constants Variable Name Check ====>\n\n'
+echo "Rule - 7: Constants Variable Name Check ====>\n\n" >> target/PMC_FileScanLogs.txt
 for mf in "${modifiedFilesList[@]}"
 do
     noWarningsFlag=true
@@ -128,6 +143,7 @@ do
             then
                 noWarningsFlag=false
                 printf "[WARNING] Please use uppercase constant names: ==>> "$variableName' <<== in the file:'$mf'\n'
+                echo "[WARNING] Please use uppercase constant names: ==>> "$variableName' <<== in the file:'$mf'\n' >> target/PMC_FileScanLogs.txt
             fi
             IFS=$'\n'
         done
@@ -135,6 +151,7 @@ do
 
 done
 printf "\n------------------------------------------------------------------------------------\n"
+echo "\n------------------------------------------------------------------------------------\n" >> target/PMC_FileScanLogs.txt
 
 # Grep Method Names, which has underscore in its name >>
 # grep -n ".*_.*[\{]" src/test/java/DevOpsDemo/CalculatorTestIT.java
@@ -145,9 +162,10 @@ printf "\n----------------------------------------------------------------------
 
 # Rule - 5: Camel case variable naming standards
 printf 'Rule - 5 & 6: Camel case variable/method naming standards ====>\n\n'
+echo 'Rule - 5 & 6: Camel case variable/method naming standards ====>\n\n' >> target/PMC_FileScanLogs.txt
 for mf in "${modifiedFilesList[@]}"
 do
-    noWarningsFlag=true
+    warningsFlag=false
     # check if file exists and extension is java
     if [[  -f "$mf" && "$mf" == *java ]];
     then
@@ -156,16 +174,24 @@ do
 
         for mLine in ${variableNames[@]}
         do
-            printf "[WARNING] Use camelCase variable Names without undescores: ==>> "$mLine' <<== in the file:'$mf'\n'
+            printf "[WARNING] Use camelCase variable Names without underscores: ==>> "$mLine' <<== in the file:'$mf'\n'
+            echo "[WARNING] Use camelCase variable Names without underscores: ==>> "$mLine' <<== in the file:'$mf'\n' >> target/PMC_FileScanLogs.txt
+            warningsFlag=true
         done
 
         methodNames=($(grep -n ".*_.*{" "$mf"))
         for mLine in ${methodNames[@]}
         do
-            printf "[WARNING] Use camelCase method Names without undescores: ==>> "$mLine' <<== in the file:'$mf'\n'
+            printf "[WARNING] Use camelCase method Names without underscores: ==>> "$mLine' <<== in the file:'$mf'\n'
+            echo "[WARNING] Use camelCase method Names without underscores: ==>> "$mLine' <<== in the file:'$mf'\n' >> target/PMC_FileScanLogs.txt
+            warningsFlag=true
         done
 
-        printf "\n"
+        if [[ $warningsFlag == true  ]];
+        then
+            printf "\n"
+            echo "\n" >> target/PMC_FileScanLogs.txt
+        fi
     fi
 
 done
