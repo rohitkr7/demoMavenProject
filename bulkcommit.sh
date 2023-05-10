7fc5894e3da6ae7fbabea352bbb21e467f6aa284
rohitkr7 <rohitkrroy06@gmail.com>
2023-05-10 23:30:53 +0530

    1:dev_randfile_2023_05_10_23_30_1683741653.txt

--- bulkcommit.sh
+++ bulkcommit.sh
@@ -2,18 +2,18 @@
 ## Current Working Directory ##
 echo "Present Working Directory is >> "$PWD
 
-#I need to write code logic for deleting/creating folders for the bulk commit
-echo "Do you want to re-create(previous data will be shreaded) 'BulkCommitDir' folder? (y/n) >> "
-read createNewDirectoryFlag
+# #I need to write code logic for deleting/creating folders for the bulk commit
+# echo "Do you want to re-create(previous data will be shreaded) 'BulkCommitDir' folder? (y/n) >> "
+# read createNewDirectoryFlag
 
-if [ ${createNewDirectoryFlag} == "y" ] || [ ${createNewDirectoryFlag} == "yes" ]; then
-    #echo "your answer is '$createNewDirectoryFlag'"
-    directoryRecreateCmd="rm -rf BulkCommitDir && mkdir BulkCommitDir"
-    echo "Recreating 'BulkCommitDir' folder........."
-    eval "$directoryRecreateCmd"
-else
-    echo "Folder Recreation skipped...."
-fi
+# if [ ${createNewDirectoryFlag} == "y" ] || [ ${createNewDirectoryFlag} == "yes" ]; then
+#     #echo "your answer is '$createNewDirectoryFlag'"
+#     directoryRecreateCmd="rm -rf BulkCommitDir && mkdir BulkCommitDir"
+#     echo "Recreating 'BulkCommitDir' folder........."
+#     eval "$directoryRecreateCmd"
+# else
+#     echo "Folder Recreation skipped...."
+# fi
 
 # Function to create multiple commits
 createNewCommits() {
@@ -39,15 +39,17 @@ createNewCommits() {
 }
 
 # Conditional Check for the total commits to be done.
-echo "Enter, How many new commits you want? "
+echo "How many new commits you want? "
 read totalCommitCount
 
 if [ ${totalCommitCount} -le 0 ]; then
     echo "Zero or Negative Input, No commits to happen."
-elif [ ${totalCommitCount} -gt 5000 ]; then
-    echo "Input is more than 5000, Please input a positive integer less than 5000"
+elif [ ${totalCommitCount} -gt 20000 ]; then
+    echo "Input is more than 20000, Please input a positive integer less than 5000"
 else
     echo "'${totalCommitCount}' new commits in progress."
+    #clearing the previous files everytime we hit this shell script freshly
+    directoryRecreateCmd="rm -rf BulkCommitDir && mkdir BulkCommitDir"
     createNewCommits
     echo "'${totalCommitCount}' new commits are created..... "
 fi
